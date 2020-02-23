4695db9899b56a73fd1e14b62d41aa6961322451
kenjhn <kennedy.ibeh@telesoftas.com>
2020-02-23 11:45:58 +0200

    feat: added LegendaryItem with barebone features

---
+++ Java/src/main/java/com/gildedrose/itemtypes/LegendaryItem.java
@@ -0,0 +1,16 @@
+package com.gildedrose.itemtypes;
+
+import com.gildedrose.Item;
+
+public class LegendaryItem extends ItemWrapper {
+
+    public LegendaryItem(Item item) {
+        super(item);
+    }
+
+    @Override
+    public void updateQuality() {
+
+    }
+
+}
