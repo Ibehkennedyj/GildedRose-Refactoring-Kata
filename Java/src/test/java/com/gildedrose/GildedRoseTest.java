d36b6e0917ab3b9f3d781081385fc04949994bff
kenjhn <kennedy.ibeh@telesoftas.com>
2020-02-23 19:14:24 +0200

    chore: changed the item used in GildedRose

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,6 +1,5 @@
 package com.gildedrose;
 
-import com.gildedrose.decorators.ItemDecorator;
 import com.gildedrose.itemtypes.ItemWrapper;
 import org.junit.jupiter.api.Test;
 
@@ -8,9 +7,9 @@ class GildedRoseTest {
 
     @Test
     void new_method_should_compile() {
-        ItemDecorator[] wrappedItems = new ItemWrapper[]{};
+        ItemWrapper[] wrappedItems = new ItemWrapper[]{};
         GildedRose app = new GildedRose(wrappedItems);
-        app.newUpdateQuality();
+        app.updateQuality();
     }
 
 }
