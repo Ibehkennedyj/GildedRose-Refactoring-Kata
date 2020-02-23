e970160b19c4d743a084a957940e7b95a0bed8dd
kenjhn <kennedy.ibeh@telesoftas.com>
2020-02-23 11:49:52 +0200

    feat: added BackstagePasses item type

---
+++ Java/src/main/java/com/gildedrose/itemtypes/BackstagePasses.java
@@ -0,0 +1,30 @@
+package com.gildedrose.itemtypes;
+
+import com.gildedrose.Item;
+import com.gildedrose.RateMap;
+import com.gildedrose.features.QualityChangeRateFluctuates;
+import com.gildedrose.features.QualityIncreases;
+import com.gildedrose.features.QualityIsZeroAfterSellIn;
+
+import java.util.HashMap;
+
+public class BackstagePasses extends ItemWrapper implements QualityChangeRateFluctuates, QualityIsZeroAfterSellIn, QualityIncreases {
+
+    public BackstagePasses(Item item) {
+        super(item);
+    }
+
+    @Override
+    public int getIncreaseRate() {
+        return getCurrentRate();
+    }
+
+    @Override
+    public RateMap getChangeRateMap() {
+        return new RateMap(1, new HashMap<Integer, Integer>(){{
+            put(10, 2);
+            put(5, 3);
+        }});
+    }
+
+}
