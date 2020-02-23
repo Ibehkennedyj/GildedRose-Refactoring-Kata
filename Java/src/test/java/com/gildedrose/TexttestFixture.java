259846ce6445897332dccbf3ab36e89ba044172a
kenjhn <kennedy.ibeh@telesoftas.com>
2020-02-23 11:51:33 +0200

    chore: populate newRun with a duplicate of existingRun but with ItemWrappers for each of the defined item

--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -35,7 +35,37 @@ public class TexttestFixture {
             builder.append("\n");
             app.updateQuality();
         }
+
+        return builder.toString();
     }
+
+    public static String newRun(int days) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("OMGHAI!").append("\n");
+
+        ItemDecorator[] items = new ItemDecorator[]{
+                new RegularItem(new Item("+5 Dexterity Vest", 10, 20)), //
+                new AgedBrie(new Item("Aged Brie", 2, 0)), //
+                new RegularItem(new Item("Elixir of the Mongoose", 5, 7)), //
+                new LegendaryItem(new Item("Sulfuras, Hand of Ragnaros", 0, 80)), //
+                new LegendaryItem(new Item("Sulfuras, Hand of Ragnaros", -1, 80)),
+                new BackstagePasses(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)),
+                new BackstagePasses(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49)),
+                new BackstagePasses(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)),
+                // this conjured item does not work properly yet
+                new RegularItem(new Item("Conjured Mana Cake", 3, 6))};
+
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < days; i++) {
+            builder.append("-------- day ").append(i).append(" --------").append("\n");
+            builder.append("name, sellIn, quality").append("\n");
+            for (ItemDecorator decorator : items) {
+                builder.append(decorator.getItem()).append("\n");
+            }
+            builder.append("\n");
+            app.newUpdateQuality();
+        }
 
         return builder.toString();
     }
