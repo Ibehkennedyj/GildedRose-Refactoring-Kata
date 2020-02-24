366a39989c99da19447f4ad900d7f886ed09a9cb
kenjhn <kennedy.ibeh@telesoftas.com>
2020-02-24 13:17:45 +0200

    chore: changed writing tool to StringBuilder

--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,10 +1,11 @@
 package com.gildedrose;
 
 public class TexttestFixture {
-    public static void main(String[] args) {
-        System.out.println("OMGHAI!");
+    public static void main(String... args) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("OMGHAI!").append("\n");
 
-        Item[] items = new Item[] {
+        Item[] items = new Item[]{
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("Aged Brie", 2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
@@ -18,20 +19,22 @@ public class TexttestFixture {
 
         GildedRose app = new GildedRose(items);
 
-        int days = 2;
+        int days = 20;
         if (args.length > 0) {
             days = Integer.parseInt(args[0]) + 1;
         }
 
         for (int i = 0; i < days; i++) {
-            System.out.println("-------- day " + i + " --------");
-            System.out.println("name, sellIn, quality");
+            builder.append("-------- day ").append(i).append(" --------").append("\n");
+            builder.append("name, sellIn, quality").append("\n");
             for (Item item : items) {
-                System.out.println(item);
+                builder.append(item).append("\n");
             }
-            System.out.println();
+            builder.append("\n");
             app.updateQuality();
         }
+
+        System.out.println(builder.toString());
     }
 
 }
